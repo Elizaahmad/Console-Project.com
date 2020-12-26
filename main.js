03f706f7256d5be536ee7d4008ce76be392165e3
Elizaahmad <74451187+Elizaahmad@users.noreply.github.com>
2020-12-26 23:20:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var header = "My First Console Project!";
+
+    console.log(header);
+
+    var first_name = "Aryan";
+
+    var last_name = " Serrao";
+
+    var name = first_name.concat(last_name);
+
+    console.log(name);
+
+    var number1 = 72;
+
+    console.log(number1);
+
+    var number2 = 5;
+
+    console.log(number2);
+
+    var add = number1 + number2;
+
+    console.log(add);
+
+    var subtract = number1 - number2;
+
+    console.log(subtract);
+
+    var multiply = number1 * number2;
+
+    console.log(multiply);
+
+    var divide = number1 / number2;
+
+    console.log(divide);
+
+    var instruct = "INSTRUCTIONS:";
+
+    console.log(instruct);
+
+    var instructions = "The First 2 Numbers Are The Number That I Used And, The Last 4 Numbers Are The Calculations :";
+
+    console.log(instructions);
+
+    console.log("1 - ADDITION");
+
+    console.log("2 - SUBTRACTION");
+
+    console.log("3 - MULTIPLICATION");
+
+    console.log("4 - DIVISION");
